b560077e6f99fbf2f6331823881f261a60d8696c
Minsup Lee <m1nsuppp@gmail.com>
2025-02-16 02:24:54 +0900

    feat: init app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import m1nsupppConfig from '@m1nsuppp/eslint-config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...m1nsupppConfig,
 ];
 
 export default eslintConfig;
---
+++ prettier.config.mjs
@@ -0,0 +1,4 @@
+import m1nsupppPrettierConfig from "@m1nsuppp/prettier-config";
+
+/** @type {import('prettier').Config} */
+export default m1nsupppPrettierConfig;
